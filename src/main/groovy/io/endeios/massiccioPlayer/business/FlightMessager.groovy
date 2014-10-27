e34f8a01674d69c0cc3e87cd49ee7ae5c245f237
Endeios <endeios@gmail.com>
2014-10-27 22:45:09 +0100

    changed class name

--- src/main/groovy/io/endeios/massiccioPlayer/business/FlightMessager.groovy
+++ src/main/groovy/io/endeios/massiccioPlayer/business/FlightMessager.groovy
@@ -3,7 +3,7 @@ package io.endeios.massiccioPlayer.business
 import io.endeios.massiccioPlayer.model.messages.*
 
 import java.util.Observable;
-public class FlightMessage extends Observable
+public class FlightMessager extends Observable
 {
 	public startTime(){
 		setChanged()
